506dadaee02a9049598e6f761f8e116b43f66a5c
Caiting Wu <mrctwu@gmail.com>
2018-04-15 21:21:48 -0500

    Milestone 3 Need for Testing

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -4,11 +4,15 @@
 
 #include <mxnet/base.h>
 
+// Set the tile width, use 16 for now since it is suggested in the textbook
+#define TILE_WIDTH 16
+
 namespace mxnet
 {
 namespace op
 {
 
+// The kernel code
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
@@ -21,24 +25,52 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    int n, m, c, h, w, p, q;
+    int h0, w0, h_base, w_base;
+    int W_grid = W_out / TILE_WIDTH;
 
-    
+    // Since each thread block will be responsible for computing one 16x16 tile in the output
+    // We need to set some of the parameters
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h0 = blockIdx.x;
+    w0 = blockIdx.y;
 
-#undef y4d
-#undef x4d
-#undef k4d
+    h_base = (blockIdx.z / W_grid) * TILE_WIDTH;
+    w_base = (blockIdx.z % W_grid) * TILE_WIDTH;
+
+    h = h_base + h0;
+    w = w_base + w0;
+
+    float acc = 0.;
+
+    //(void)H_out; // silence declared but never referenced warning. remove this line when you start working
+    //(void)W_out; // silence declared but never referenced warning. remove this line when you start working
+
+    // An example use of these macros:
+    // float a = y4d(0,0,0,0)
+    // y4d(0,0,0,0) = a
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    if ((h0 < K) && (w0 < K)){
+      for (c = 0; c < C; c++) {
+        for (p = 0; p < K; p++){
+          for (q = 0; q < K; q++){
+            acc += (x4d(n, c, h + p, w + q) * k4d(n, c, p, q));
+          }
+        }
+      }
+      y4d(n, m, h, w) = acc;
+    }
+    #undef y4d
+    #undef x4d
+    #undef k4d
 }
 
-/* 
+/*
    This function is called by new-inl.h
    Any code you write should be executed by this function.
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
@@ -49,24 +81,36 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
+    const int N = x.shape[0];
+    const int M = y.shape[0];
+    const int C = x.shape[1];
+    const int H = x.shape[2];
+    const int W = x.shape[3];
+    const int K = w.shape[2];
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    const int W_grid = W_out / TILE_WIDTH;
+    const int H_grid = H_out / TILE_WIDTH;
+    const int Z = H_grid * W_grid;
+
+    dim3 gridDim(N, M, Z);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, N,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
-/* 
+/*
     This tells mxnet how to do an op when it's not a float.
     This is not used in the ECE408 project
 */
